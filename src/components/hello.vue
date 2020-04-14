ce57594880c829b0c3d81ef4141f32f7b8d8a16f
张标180326 <3283367153@qq.com>
2020-04-14 19:00:05 +0800

    上午

---
+++ src/components/hello.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>this is vue</h1>
+        <h2>{{message}}</h2>
+        <h3>{{hobbies[0]}}</h3>
+        <button @click="say">say</button>
+        <button @click="hello('hello')">hello</button>
+    </div>
+</template>>
+<script>
+export default {
+  data () {
+    return {
+      message: 'hello vue',
+      hobbies: ['爬山', '游泳', '跑步', '羽毛球']
+    }
+  },
+  methods: {
+    say: function () {
+      alert('hello nihao')
+    },
+    hello: function (newMessage) {
+      this.message = newMessage
+      console.log(this.message)
+    }
+  }
+}
+</script>
+<style scoped>
+    h1{
+        font-size: 30px;
+        color: red;
+    }
+</style>
